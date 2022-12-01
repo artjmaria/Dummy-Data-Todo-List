b5804458365c672cca421fd3927b826a3cb43061
artjmaria <artjmaria@gmail.com>
2022-11-30 23:34:31 -0600

    alternate for loop solution added

--- scripts.js
+++ scripts.js
@@ -36,11 +36,11 @@
 
 
     const logTodos = () => {
-      // console.log(arrayOfTodos);
-      console.log(arrayOfTodos[0].userId) // => 1
-      console.log(arrayOfTodos[1].userId) // => 1
-      console.log(arrayOfTodos[2].userId) // => 1
-      console.log(arrayOfTodos[3].userId) // => 1
+      console.log(arrayOfTodos);
+      // console.log(arrayOfTodos[0].userId) // => 1
+      // console.log(arrayOfTodos[1].userId) // => 1
+      // console.log(arrayOfTodos[2].userId) // => 1
+      // console.log(arrayOfTodos[3].userId) // => 1
 
     }
 
@@ -48,14 +48,28 @@
       for (let i = 0; i < arrayOfTodos.length; i++) {
 
 
-        const tdList = document.getElementById('todo-list')
+        const olList = document.getElementById('todo-list')
         const li = document.createElement('li')
         const textNode = document.createTextNode(arrayOfTodos[i].title)
-        tdList.appendChild(li)
+        olList.appendChild(li)
         li.appendChild(textNode)
 
       }
     }
+
+    // (ADDITIONAL PRACTICE CODE/ ALTERNATIVE WAY TO APPROACH PROBLEM)
+
+    // const populateTodos = () => {
+
+    //   const tdList = document.getElementById('todo-list')
+    //   let lis = ``
+    //   for (let i = 0; i < arrayOfTodos.length; i++) {
+    //     lis += `<li>Title of To-Do: ${arrayOfTodos[i].title}</li>`
+
+    //   }
+    //   console.log(lis)
+    //   tdList.innerHTML = lis
+    // }
 
 
     // const populateTodos = () => {
@@ -68,12 +82,20 @@
     // }
 
 
-    // (ADDITIONAL PRACTICE CODE)
-
-    // const populateTodos = () => {
 
 
-    //   const tdList = document.getElementById('todo-list')
-    //   tdList.innerHTML= `<li>Title of To-Do: ${arrayOfTodos[0].title}</li>`
 
-    // }
+
+    // // const words = ['ray', 'limitless', 'elite', 'exuberant', 'devotion', 'presence', 'great'];
+
+    // // const result = words.filter(word => word.length > 6);
+
+    // // console.log(result);
+    // // Try it yourself, paste this into your browser's console, repl.it, or in a new repo and let it fly
+
+    // const form = document.querySelector("form")
+    // form.addEventListener("submit", e => {
+    //     e.preventDefault();
+
+    // ol.innerHTML = ""
+    // })
