ab900577d2baefc2c05f0ba7bc816f7391afc1da
artjmaria <artjmaria@gmail.com>
2022-12-05 22:32:35 -0600

    final edits and function checks

--- scripts.js
+++ scripts.js
@@ -1,101 +1,149 @@
-    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
-
-    let arrayOfTodos = [{
-        "userId": 1,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-      },
-      {
-        "userId": 1,
-        "id": 2,
-        "title": "quis ut nam facilis et officia qui",
-        "completed": false
-      },
-      {
-        "userId": 1,
-        "id": 3,
-        "title": "fugiat veniam minus",
-        "completed": false
-      },
-      {
-        "userId": 1,
-        "id": 4,
-        "title": "et porro tempora",
-        "completed": true
-      }
-    ]
+let arrayOfTodos = [];
 
 
+const fetchTodos = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.json())
+    .then((data) => arrayOfTodos = data)
+}
 
-    const fetchTodos = () => {
-      fetch('https://jsonplaceholder.typicode.com/todos')
-        .then((response) => response.json())
-        .then((json) => arrayOfTodos = json)
+const logTodos = () => {
+  console.log(arrayOfTodos);
+}
+
+// =============================================================================
+// ALTERNATE WAY #1
+
+// const populateTodos = () => {
+//   for (let i = 0; i < arrayOfTodos.length; i++) {
+
+//     const olList = document.getElementById('todo-list')
+//     const li = document.createElement('li')
+//     const textNode = document.createTextNode(arrayOfTodos[i].title)
+//     olList.appendChild(li)
+//     li.appendChild(textNode)
+
+//   }
+// }
+// =================================================================================
+
+// ALTERNATE WAY #2
+
+// (ADDITIONAL PRACTICE CODE/ ALTERNATIVE WAY TO APPROACH PROBLEM)
+
+// const populateTodos = () => {
+
+//   const tdList = document.getElementById('todo-list')
+//   let lis = ``
+//   for (let i = 0; i < arrayOfTodos.length; i++) {
+//     lis += `<li>Title of To-Do: ${arrayOfTodos[i].title}</li>`
+
+//   }
+//   console.log(lis)
+//   tdList.innerHTML = lis
+// }
+
+// const populateTodos = () => {
+//   const tdList = document.getElementById('todo-list')
+//   const li = document.createElement('li')
+//   const textNode = document.createTextNode(arrayOfTodos[0].title)
+//   tdList.appendChild(li)
+//   li.appendChild(textNode)
+
+// }
+// =========================================================================================
+
+const populateTodos = () => {
+
+  document.getElementById("todo-list").innerHTML = "";
+
+  for (let i = 0; i < arrayOfTodos.length; i++) {
+
+
+    const listMain = document.getElementById("todo-list");
+
+    const newItem = document.createElement("li");
+
+    const newData = document.createTextNode(arrayOfTodos[i].title);
+
+    newItem.appendChild(newData);
+    listMain.appendChild(newItem);
+
+    if (arrayOfTodos[i].completed === false) {
+      newItem.style.color = 'red';
+    } else {
+      newItem.style.textDecoration = "line-through";
     }
+  }
+}
+
+const clearTodos = () => {
+  document.getElementById("todo-list").innerHTML = "";
+}
+
+const filterTodos = () => {
+
+  document.getElementById("todo-list").innerHTML = "";
+
+  const idNumber = document.getElementById("dropdown").value;
+  console.log(idNumber);
+
+  const filteredArray = arrayOfTodos.filter(todo => todo.userId == idNumber);
+  console.log(filteredArray);
 
 
-    const logTodos = () => {
-      console.log(arrayOfTodos);
-      // console.log(arrayOfTodos[0].userId) // => 1
-      // console.log(arrayOfTodos[1].userId) // => 1
-      // console.log(arrayOfTodos[2].userId) // => 1
-      // console.log(arrayOfTodos[3].userId) // => 1
+  for (let i = 0; i < filteredArray.length; i++) {
 
-    }
+    const listMain = document.getElementById("todo-list");
 
-    const populateTodos = () => {
-      for (let i = 0; i < arrayOfTodos.length; i++) {
+    const newItem = document.createElement("li");
 
+    const newData = document.createTextNode(filteredArray[i].title);
 
-        const olList = document.getElementById('todo-list')
-        const li = document.createElement('li')
-        const textNode = document.createTextNode(arrayOfTodos[i].title)
-        olList.appendChild(li)
-        li.appendChild(textNode)
+    newItem.appendChild(newData);
+    listMain.appendChild(newItem);
 
-      }
-    }
+  }
+}
 
-    // (ADDITIONAL PRACTICE CODE/ ALTERNATIVE WAY TO APPROACH PROBLEM)
+const completeTodos = () => {
 
-    // const populateTodos = () => {
+  document.getElementById("todo-list").innerHTML = "";
 
-    //   const tdList = document.getElementById('todo-list')
-    //   let lis = ``
-    //   for (let i = 0; i < arrayOfTodos.length; i++) {
-    //     lis += `<li>Title of To-Do: ${arrayOfTodos[i].title}</li>`
+  const filteredArray = arrayOfTodos.filter(todo => todo.completed === true);
+  console.log(filteredArray);
 
-    //   }
-    //   console.log(lis)
-    //   tdList.innerHTML = lis
-    // }
+  for (let i = 0; i < filteredArray.length; i++) {
 
+    const listMain = document.getElementById("todo-list");
 
-    // const populateTodos = () => {
-    //   const tdList = document.getElementById('todo-list')
-    //   const li = document.createElement('li')
-    //   const textNode = document.createTextNode(arrayOfTodos[0].title)
-    //   tdList.appendChild(li)
-    //   li.appendChild(textNode)
+    const newItem = document.createElement("li");
+    newItem.style.textDecoration = "line-through";
 
-    // }
+    const newData = document.createTextNode(filteredArray[i].title);
 
+    newItem.appendChild(newData);
+    listMain.appendChild(newItem);
+  }
+}
 
+const incompleteTodos = () => {
 
+  document.getElementById("todo-list").innerHTML = "";
 
+  const filteredArray = arrayOfTodos.filter(todo => todo.completed === false);
+  console.log(filteredArray);
 
+  for (let i = 0; i < filteredArray.length; i++) {
 
-    // // const words = ['ray', 'limitless', 'elite', 'exuberant', 'devotion', 'presence', 'great'];
+    const listMain = document.getElementById("todo-list");
 
-    // // const result = words.filter(word => word.length > 6);
+    const newItem = document.createElement("li");
+    newItem.style.color = 'red';
 
-    // // console.log(result);
-    // // Try it yourself, paste this into your browser's console, repl.it, or in a new repo and let it fly
+    const newData = document.createTextNode(filteredArray[i].title);
 
-    // const form = document.querySelector("form")
-    // form.addEventListener("submit", e => {
-    //     e.preventDefault();
-
-    // ol.innerHTML = ""
-    // })
+    newItem.appendChild(newData);
+    listMain.appendChild(newItem);
+  }
+}
